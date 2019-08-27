a2d460e67a1b7354d4ce57a2208ea10dee3d7427
Jango Course <jango.course@gmail.com>
2019-08-27 11:27:46 +0200

    Initial commit. Added modinfo.lua and a LICENSE file (QPL-1.0).

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+
+meta =
+{
+    id = "DeathInfo",
+    name = "DeathInfo",
+    title = "Death Info",
+    description = "When a personal ship of yours or one of your Alliance gets destroyed, you will receive a Mail describing the location of its death, and who killed it.",
+
+    authors = {"Shrooblord"},
+    version = "1.0",
+
+    dependencies = {
+        {id = "Avorion", min = "0.26"},
+    },
+
+    serverSideOnly = false,
+    clientSideOnly = false,
+    saveGameAltering = false,
+
+    contact = "avorion@shrooblord.com",
+}
